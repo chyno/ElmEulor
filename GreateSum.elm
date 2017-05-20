94b6141e1196ddc03ceca59b02e2648b08b27956
Joh Chynoweth <jchynowe1@yahoo.com>
2017-05-19 23:06:14 -0400

    max summ

--- GreateSum.elm
+++ GreateSum.elm
@@ -1,7 +1,23 @@
 import Html exposing (text)
  
-data : List (Int, Int)
-data = List.range 1 64 |> List.indexedMap (\ a b -> (a + 1, b))
+cubesize : Int
+cubesize = 8
 
-main =
-  text (toString data)
+sumsize : Int
+sumsize = 4
+
+vldrow : Int -> Bool
+vldrow index =
+  let
+    remainder =(index % cubesize)
+  in
+    sumsize <= (cubesize - remainder)   &&  (remainder > 0)
+  
+data : List (Int, Int)
+data = List.range 1 (cubesize^2) |> List.indexedMap (\ a b -> (a + 1, b))
+
+rowdata : List (Int, Int)
+rowdata = List.filter (\ (index, val) ->  vldrow index) data
+
+main = 
+  text (toString (rowdata))
