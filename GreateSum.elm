8d29f23ab66a8c439cf50e6a0e074e89ef653e07
Joh Chynoweth <jchynowe1@yahoo.com>
2017-05-21 13:04:13 -0400

    row sum for greatest sum

--- GreateSum.elm
+++ GreateSum.elm
@@ -6,18 +6,39 @@ cubesize = 8
 sumsize : Int
 sumsize = 4
 
-vldrow : Int -> Bool
-vldrow index =
+validcell : Int -> Bool
+validcell index =
   let
     remainder =(index % cubesize)
   in
-    sumsize <= (cubesize - remainder)   &&  (remainder > 0)
+    (sumsize <= (cubesize - remainder) + 1)   &&  (remainder > 0)
   
 data : List (Int, Int)
 data = List.range 1 (cubesize^2) |> List.indexedMap (\ a b -> (a + 1, b))
 
-rowdata : List (Int, Int)
-rowdata = List.filter (\ (index, val) ->  vldrow index) data
+rowsumitem : (Int, Int) ->  Int
+rowsumitem (index, val) =
+  data |>
+  List.filter (\ (a, b) -> a >= index && a < (index + 4) )  |>
+  List.map (\ (a, b)-> b ) |> 
+  List.sum
+
+
+filterdata : List Int
+filterdata =
+  data |>
+  List.filter (\ (index, val) ->  validcell index)  |>
+  List.map rowsumitem
+
+answer : Int
+answer = 
+  filterdata |> 
+  List.maximum  |>
+  (\ ans ->
+     case ans of
+       Nothing -> 0
+       Just x -> x
+  )
 
 main = 
-  text (toString (rowdata))
+  text (toString (answer))
